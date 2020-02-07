6be8b2d98741eca30979c10fc676cd5b53b9b9d4
yogesh-hullatti <yogesh.hullatti@hmcts.net>
2020-02-07 10:57:07 +0000

    Added in changes for openId Connect

---
+++ src/main/java/uk/gov/hmcts/reform/em/idam/client/models/test/CreateUserRequest.java
@@ -0,0 +1,29 @@
+package uk.gov.hmcts.reform.em.idam.client.models.test;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@Builder
+public class CreateUserRequest {
+
+    public static final String DEFAULT_PASSWORD = "Password12";
+
+    private String email;
+    @Builder.Default
+    private String forename = "John";
+    @Builder.Default
+    private String surname = "Smith";
+    @Builder.Default
+    private Integer levelOfAccess = 0;
+    private UserGroup userGroup;
+    private List<UserRole> roles;
+    @Builder.Default
+    private String activationDate = "";
+    @Builder.Default
+    private String lastAccess = "";
+    @Builder.Default
+    private String password = DEFAULT_PASSWORD;
+}

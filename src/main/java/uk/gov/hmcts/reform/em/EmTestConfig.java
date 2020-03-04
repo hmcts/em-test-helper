2e1dc33557adbd3a41002a604d7468bc5d5afab7
yogesh-hullatti <yogesh.hullatti@hmcts.net>
2020-03-04 11:00:35 +0000

    Reverted the proxy related changes

--- src/main/java/uk/gov/hmcts/reform/em/EmTestConfig.java
+++ src/main/java/uk/gov/hmcts/reform/em/EmTestConfig.java
@@ -4,11 +4,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
-
-import java.net.InetSocketAddress;
-import java.net.Proxy;
 
 
 @Configuration
@@ -18,12 +14,7 @@ public class EmTestConfig {
 
     @Bean
     RestTemplate restTemplate() {
-        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
-
-        Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("proxyout.reform.hmcts.net", 8080));
-        requestFactory.setProxy(proxy);
-
-        return new RestTemplate(requestFactory);
+        return new RestTemplate();
     }
 
 }
--- src/main/java/uk/gov/hmcts/reform/em/test/idam/IdamConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/em/test/idam/IdamConfiguration.java
@@ -1,17 +1,12 @@
 package uk.gov.hmcts.reform.em.test.idam;
 
-import okhttp3.OkHttpClient;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 import uk.gov.hmcts.reform.idam.client.IdamClient;
 import uk.gov.hmcts.reform.idam.client.IdamTestApi;
-
-import java.net.InetSocketAddress;
-import java.net.Proxy;
 
 @Configuration
 @ConditionalOnProperty("idam.api.url")
@@ -22,16 +17,5 @@ public class IdamConfiguration {
     @Bean
     IdamHelper idamHelper(IdamClient idamClient, IdamTestApi idamTestApi, DeleteUserApi deleteUserApi) {
         return new IdamHelper(idamClient, idamTestApi, deleteUserApi);
-    }
-
-    @Bean
-    @Scope("prototype")
-    public OkHttpClient client() {
-
-        OkHttpClient okHttpClient = new OkHttpClient.Builder()
-                .proxy(new Proxy(Proxy.Type.HTTP, new InetSocketAddress("proxyout.reform.hmcts.net", 8080)))
-                .build();
-
-        return okHttpClient;
     }
 }

f010043a058052b421c336150df5a5eaeb269b5a
yogesh-hullatti <yogesh.hullatti@hmcts.net>
2021-10-15 15:51:47 +0100

    Updated Xui-Webapp & CCD S2S client-id as optional

--- src/aat/java/uk/gov/hmcts/reform/em/functional/S2sScenario.java
+++ src/aat/java/uk/gov/hmcts/reform/em/functional/S2sScenario.java
@@ -1,13 +1,17 @@
 package uk.gov.hmcts.reform.em.functional;
 
+import org.junit.Assume;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import uk.gov.hmcts.reform.em.EmTestConfig;
 import uk.gov.hmcts.reform.em.test.s2s.S2sHelper;
+
+import java.util.Objects;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -19,10 +23,12 @@ public class S2sScenario {
     @Autowired
     S2sHelper s2sHelper;
 
-    @Autowired
+    @Autowired(required = false)
+    @Qualifier("ccdS2sHelper")
     S2sHelper ccdS2sHelper;
 
-    @Autowired
+    @Autowired(required = false)
+    @Qualifier("xuiS2sHelper")
     S2sHelper xuiS2sHelper;
 
     @Test
@@ -32,11 +38,13 @@ public class S2sScenario {
 
     @Test
     public void getCcdS2sToken() {
+        Assume.assumeTrue(Objects.nonNull(ccdS2sHelper));
         assertThat(ccdS2sHelper.getS2sToken()).isNotEmpty();
     }
 
     @Test
     public void getXuiS2sToken() {
+        Assume.assumeTrue(Objects.nonNull(xuiS2sHelper));
         assertThat(xuiS2sHelper.getS2sToken()).isNotEmpty();
     }
 }

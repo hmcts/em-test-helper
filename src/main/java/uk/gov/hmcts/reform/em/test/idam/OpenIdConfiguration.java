6afa7cca935f355e2ac5cb8123ecb65be7705802
mustafa <mdayican@gmail.com>
2024-05-29 15:07:38 +0100

    EM-6046 fix idam auth-2 (#436)

--- src/main/java/uk/gov/hmcts/reform/em/test/idam/OpenIdConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/em/test/idam/OpenIdConfiguration.java
@@ -3,25 +3,22 @@ package uk.gov.hmcts.reform.em.test.idam;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
-
-
-@SuppressWarnings({"ParameterName","MemberName"})
 @Configuration
 public class OpenIdConfiguration {
 
     private String clientId;
-    private String clientSecret;
+    private String client_secret;
     private String redirectUri;
     private String grant_type;
     private String scope;
 
     public OpenIdConfiguration(@Value("${idam.client.id:}") String clientId,
-                               @Value("${idam.client.secret:}") String clientSecret,
+                               @Value("${idam.client.secret:}") String client_secret,
                                @Value("${idam.client.redirect_uri:}") String redirectUri,
                                @Value("${idam.client.scope:}") String scope,
                                @Value("${idam.client.grant_type:}") String grant_type) {
         this.clientId = clientId;
-        this.clientSecret = clientSecret;
+        this.client_secret = client_secret;
         this.redirectUri = redirectUri;
         this.grant_type = grant_type;
         this.scope = scope;
@@ -32,7 +29,7 @@ public class OpenIdConfiguration {
     }
 
     public String getClient_secret() {
-        return clientSecret;
+        return client_secret;
     }
 
     public String getRedirectUri() {
--- src/test/java/uk/gov/hmcts/reform/em/test/IdamHelperTest.java
+++ src/test/java/uk/gov/hmcts/reform/em/test/IdamHelperTest.java
@@ -1,9 +1,16 @@
 package uk.gov.hmcts.reform.em.test;
 
+
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import org.mockito.junit.MockitoJUnitRunner;
 import uk.gov.hmcts.reform.em.test.idam.DeleteUserApi;
 import uk.gov.hmcts.reform.em.test.idam.IdamHelper;
@@ -14,18 +21,9 @@ import uk.gov.hmcts.reform.em.test.idam.client.models.OpenIdAuthUserResponse;
 import uk.gov.hmcts.reform.idam.client.IdamClient;
 import uk.gov.hmcts.reform.idam.client.IdamTestApi;
 import uk.gov.hmcts.reform.idam.client.models.UserDetails;
-import uk.gov.hmcts.reform.idam.client.models.UserInfo;
 
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
 
 @RunWith(MockitoJUnitRunner.class)
 public class IdamHelperTest {
@@ -64,11 +62,10 @@ public class IdamHelperTest {
     @Test
     public void testGetUserId() {
         UserDetails userDetailsMock = mock(UserDetails.class);
-        UserInfo userInfoMock = mock(UserInfo.class);
         when(userDetailsMock.getId()).thenReturn("id");
         when(openIdAuthUserResponse.getAccessToken()).thenReturn("b");
         when(openIdUserApi.authenticateUser(any())).thenReturn(openIdAuthUserResponse);
-        when(idamClient.getUserInfo("Bearer b")).thenReturn(userInfoMock);
+        when(idamClient.getUserDetails("Bearer b")).thenReturn(userDetailsMock);
         assertThat(idamHelper.getUserId("x")).isEqualTo("id");
     }
 
@@ -88,14 +85,5 @@ public class IdamHelperTest {
         verify(openIdUserApi, times(1)).authenticateUser(any());
     }
 
-    @Test
-    public void testAuthenticateUserWithPassword() {
-        when(openIdAuthUserResponse.getAccessToken()).thenReturn("b");
-        when(openIdUserApi.authenticateUser(any())).thenReturn(openIdAuthUserResponse);
-        assertThat(idamHelper.authenticateUser("x", "password")).isEqualTo("Bearer b");
-        assertThat(idamHelper.authenticateUser("x", "password")).isEqualTo("Bearer b");
-        assertThat(idamHelper.authenticateUser("x")).isEqualTo("Bearer b");
-        verify(openIdUserApi, times(1)).authenticateUser(any());
-    }
 
 }
